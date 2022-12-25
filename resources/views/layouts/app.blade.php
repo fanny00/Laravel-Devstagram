34f59c7601693e129eabdb7e5bd12fadd6bd2838
Fanny Campos Allende <fanny.campos@trebol-it.com>
2022-12-24 21:22:26 -0300

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <!--<h1>Hola mundo en Laravel Laravel v9.45.1 (PHP v8.1.13)</h1>-->
+        <a href="/">Principal</a>
+        <a href="/nosotros">Nosotros</a>
+        <a href="/tienda">Tienda</a>
+    </body>
+</html>
