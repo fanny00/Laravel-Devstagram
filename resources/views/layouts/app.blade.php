3e757b7b778c61a1f1eaf357ba4809bf30f3f07f
Fanny Campos Allende <fannycamposa@gmail.com>
2022-12-25 16:00:55 -0300

    Creando un Layout Principal y utilizandolo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,12 +4,18 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>DevStagram - @yield('titulo')</title>
     </head>
     <body>
         <!--<h1>Hola mundo en Laravel Laravel v9.45.1 (PHP v8.1.13)</h1>-->
-        <a href="/">Principal</a>
-        <a href="/nosotros">Nosotros</a>
-        <a href="/tienda">Tienda</a>
+        <a href="/">Principal</a> |
+        <a href="/nosotros">Nosotros</a> |
+        <a href="/tienda">Tienda</a> |
+        <a href="/contacto">Contacto</a>
     </body>
+    <h1>@yield('titulo')</h1>
+    <hr>
+
+    @yield('contenido')
+
 </html>
