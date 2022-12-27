20187dd211399bfcddc48402e27fe99c246dd404
Fanny Campos Allende <fannycamposa@gmail.com>
2022-12-26 23:46:39 -0300

    toda la seccion 9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,19 +3,35 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        @vite('resources/css/app.css')
+        
         <title>DevStagram - @yield('titulo')</title>
     </head>
-    <body>
-        <!--<h1>Hola mundo en Laravel Laravel v9.45.1 (PHP v8.1.13)</h1>-->
-        <a href="/">Principal</a> |
-        <a href="/nosotros">Nosotros</a> |
-        <a href="/tienda">Tienda</a> |
-        <a href="/contacto">Contacto</a>
-    </body>
-    <h1>@yield('titulo')</h1>
-    <hr>
+    <body class="bg-gray-100">
+        <header class="p-5 border-b bg-white shadow">
+            <div class="container mx-auto flex justify-between items-center">
+                <h1 class="text-3xl font-black">
+                    DevStagram
+                </h1>
+                <nav class="flex gap-2 items-center">
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Crear Cuenta</a>
+                </nav>
+            </div>
+            
+        </header>
 
-    @yield('contenido')
+        <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+
+        </main>
+        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+             DevStagram - Todos los derechos reservados {{ now()->year }}
+        </footer>
+    </body>
+    
 
 </html>
--- resources/views/tienda.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('titulo')
-    Tienda virtual
-@endsection
-
-@section('contenido')
-  Contenido de la tienda virtual
-@endsection
