035052acd98b5b729af271e8393b802584727f93
Fanny Campos Allende <fannycamposa@gmail.com>
2022-12-28 22:24:10 -0300

    Evitar Username Duplicados

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -17,6 +18,9 @@ class RegisterController extends Controller
    {
      // dd($request);
      // dd($request->get('username'));
+
+     // Modificar el Request
+     $request->request->add(['username' => Str::slug( $request->username )]);
 
      // Validación
      $this->validate($request, [
