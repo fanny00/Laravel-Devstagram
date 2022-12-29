2d3a1d47f4aad663cd48010f8f908596be7fc8f4
Fanny Campos Allende <fannycamposa@gmail.com>
2022-12-28 21:46:56 -0300

    Crear Registros con Eloquent ORM

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -25,6 +26,13 @@ class RegisterController extends Controller
         'password' => 'required|confirmed|min:6'
      ]);
 
-     dd('Creando usuario');
+     //dd('Creando usuario');
+
+     User::create([
+         'name' => $request->name,
+         'username' => $request->username,
+         'email' => $request->email,
+         'password' => $request->password
+     ]);
    }
 }
