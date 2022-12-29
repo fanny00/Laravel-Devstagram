c386e6388c3eb9a9a831633730c0d7a070d59aec
Fanny Campos Allende <fannycamposa@gmail.com>
2022-12-28 22:34:01 -0300

    Redireccionar al Usuario al Muro, una vez su cuenta es creada

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -39,5 +39,8 @@ class RegisterController extends Controller
          'email' => $request->email,
          'password' => Hash::make( $request->password ) 
      ]);
+
+     //Redireccionar
+     return redirect()->route('posts.index');
    }
 }
