07c33dc818d5e7b27564eb2663fd802d899a3820
Fanny Campos Allende <fannycamposa@gmail.com>
2023-01-01 18:52:22 -0300

    Revisar si un usuario esta autenticado o no

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,12 +13,26 @@
                 <h1 class="text-3xl font-black">
                     DevStagram
                 </h1>
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">
-                        Crear Cuenta
-                    </a>
-                </nav>
+
+                @auth
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold text-gray-600 text-sm" href="#">
+                            Hola: <span>{{ auth()->user()->username }}</span></a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" 
+                        href="{{ route('register') }}">
+                            Cerrar Sesión
+                        </a>
+                    </nav>
+                @endauth
+
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="{{ route('register') }}">
+                            Crear Cuenta
+                        </a>
+                    </nav>
+                @endguest       
             </div>
             
         </header>
