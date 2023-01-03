c8b8ddb0947857f9da2dab9ee40548eaebb5675e
Fanny Campos Allende <fannycamposa@gmail.com>
2023-01-02 23:10:28 -0300

    Finalizando el formulario

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,91 +13,43 @@
         <form action="{{ route('register') }}" method="POST" novalidate >
             @csrf
             <div class="mb-5">
-                <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Nombre
+                <label for="titulo" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Titulo
                 </label>
                 <input 
-                    id="name"
-                    name="name"
+                    id="titulo"
+                    name="titulo"
                     type="text"
-                    placeholder="Tu nombre"
-                    class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                    value="{{ old('name') }}"
+                    placeholder="Titulo de la publicación"
+                    class="border p-3 w-full rounded-lg @error('titulo') border-red-500 @enderror"
+                    value="{{ old('titulo') }}"
                 />
-                @error('name')
+                @error('titulo')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                         {{ $message }}
                     </p>
                 @enderror
             </div>
             <div class="mb-5">
-                <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Username
+                <label for="descripcion" class="mb-2 block uppercase text-gray-500 font-bold">
+                    Descripcion
                 </label>
-                <input 
-                    id="username"
-                    name="username"
-                    type="text"
-                    placeholder="Tu nombre de usuario"
-                    class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
-                    value="{{ old('username') }}"
-                />
-                @error('username')
+                <textarea
+                    id="descripcion"
+                    name="descripcion"
+                    placeholder="Descripción de la publicación"
+                    class="border p-3 w-full rounded-lg @error('descripcion') border-red-500 @enderror"
+                >{{ old('descripcion') }}</textarea>
+                @error('descripcion')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                         {{ $message }}
                     </p>
                 @enderror
             </div>
-            <div class="mb-5">
-                <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Email
-                </label>
-                <input 
-                    id="email"
-                    name="email"
-                    type="email"
-                    placeholder="Tu email de registro"
-                    class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
-                    value="{{ old('email') }}"
-                />
-                @error('email')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-            <div class="mb-5">
-                <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Password
-                </label>
-                <input 
-                    id="password"
-                    name="password"
-                    type="password"
-                    placeholder="Password de registro"
-                    class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
-                />
-                @error('password')
-                    <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-            <div class="mb-5">
-                <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
-                    Repetir Password
-                </label>
-                <input 
-                    id="password_confirmation"
-                    name="password_confirmation"
-                    type="password"
-                    placeholder="Repite tu password"
-                    class="border p-3 w-full rounded-lg"
-                />
-            </div>
+            
             <input 
                 type="submit"
-                value="Crear cuenta"
+                value="Crear publicación"
                 class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
             />
             
