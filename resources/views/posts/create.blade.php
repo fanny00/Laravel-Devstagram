e2f6bed37a52829e9f40e2157f1a4a816a6507f3
Fanny Campos <fannycamposa@gmail.com>
2023-01-25 13:22:17 -0300

    Agregar imagen a Dropzone si la validacion falla pero tenemos 1 imagen

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -63,6 +63,7 @@
                  <input
                  name="imagen"
                  type="hidden"
+                 value="{{ old('imagen') }}"
                  />
                  @error('imagen')
                     <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
