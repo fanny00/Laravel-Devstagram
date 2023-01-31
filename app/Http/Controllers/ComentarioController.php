3b00694738f379fa12b614e19a220109ab823589
Fanny Campos <fannycamposa@gmail.com>
2023-01-31 16:04:13 -0300

    Almacenando los Comentarios

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,17 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comentario;
 use Illuminate\Http\Request;
 
 class ComentarioController extends Controller
 {
-    public function store()
+    public function store(Request $request, User $user, Post $post)
     {
         // validar
+        $this->validate($request, [
+            'comentario' => 'required|max:255'
+        ]);
 
         // almacenar el resultado
+        Comentario::create([
+            'user_id' => auth()->user()->id,
+            'post_id' => $post->id,
+            'comentario' =>  $request->comentario
+        ]);
 
         // Imprimir un mensaje
-        
+         return back()->with('mensaje', 'Comentario Realizado Correctamente');
     }
 }
